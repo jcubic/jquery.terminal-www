b8c3379c8020007509eaf1cf6ee21cbad9493c50
Jakub Jankiewicz <jcubic@onet.pl>
2016-05-03 17:11:37 +0200

    Change -min to .min in index.php

--- index.php
+++ index.php
@@ -160,7 +160,7 @@ header("X-Powered-By: ");
     </footer>
     <script src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
     <script src="js/jquery.mousewheel-min.js"></script>
-    <script src="js/jquery.terminal-min.js"></script>
+    <script src="js/jquery.terminal.min.js"></script>
     <script src="js/code.js"></script>
     <script>
 
