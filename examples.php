fbe63f7ead3ac0dbe242f6c095705fef0144c789
Jakub Jankiewicz <jcubic@onet.pl>
2022-02-20 00:04:55 +0100

    add WebR to examples

--- examples.php
+++ examples.php
@@ -2151,6 +2151,7 @@ iconv -f CP437 -t UTF-8 < artwork.ans
               <li><a href="https://github.com/iodide-project/pyodide">pyodide</a> &mdash; Python with the scientific stack, compiled to WebAssembly.</li>
               <li><a href="https://try.javascript.org.pl/">JavaScript REPL</a> &mdash; JavaScript Interpreter with split view and <a href="https://eloquentjavascript.net/">Eloquent JavaScript</a> book.</li>
               <li><a href="https://github.com/stevenknox/AspNetWebCLI">AspNet Web CLI</a> &mdash; Web CLI tool for Asp.Net Core using SignalR.</li>
+              <li><a href="https://github.com/georgestagg/webR">WebR</a> &mdash; This project aims to compile the statistical language R into WASM for use in a browser, via Emscripten.</li>
             </ul>
           </li>
           <li>Interpreters, interfaces, Tools, APIs
