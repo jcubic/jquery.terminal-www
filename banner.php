73417d384592091b2b2074936a7e8c55c48b620a
Jakub Jankiewicz <jcubic@onet.pl>
2024-02-06 12:01:34 +0100

    add NameCheap banner

---
+++ banner.php
@@ -0,0 +1,10 @@
+<div class="banner">
+      <a href="https://namecheap.pxf.io/0Z3yQJ">
+        <span>Get Cheap Hosting or Domain for your Terminal App from <span class="label">NameCheap</span></span>
+        <span class="icon">
+          <svg width="55" height="30" viewBox="0 0 55 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M54.4142 16.4142C55.1953 15.6332 55.1953 14.3668 54.4142 13.5858L41.6863 0.857868C40.9052 0.0768194 39.6389 0.0768193 38.8579 0.857868C38.0768 1.63892 38.0768 2.90525 38.8579 3.68629L50.1716 15L38.8579 26.3137C38.0768 27.0948 38.0768 28.3611 38.8579 29.1421C39.6389 29.9232 40.9052 29.9232 41.6863 29.1421L54.4142 16.4142ZM-1.74846e-07 17L53 17L53 13L1.74846e-07 13L-1.74846e-07 17Z" fill="white"/>
+          </svg>
+        </span>
+      </a>
+    </div>
