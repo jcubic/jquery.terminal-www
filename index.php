76ce1ad4e48eb15e326639b5fdbffacaa591dffd
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-06-23 12:29:46 +0200

    update sentry script

--- index.php
+++ index.php
@@ -807,7 +807,13 @@ indent(`function factorial(n) {
     <script defer src="https://api.feedbhack.com/assets/app.js" website-id="670311f2ee359a44f772ffcf"></script>
     -->
     <? if ($_SERVER["HTTP_HOST"] != "localhost"): ?>
-    <script src="https://js-de.sentry-cdn.com/c6868ced9c228b7da5e50196c0ab2f14.min.js" crossorigin="anonymous"></script>
+    <script src="https://browser.sentry-cdn.com/10.59.0/bundle.tracing.replay.min.js" integrity="sha384-mZDAd4mvwMtQm+RjWbWehzkovnIw6uhmHx4+oTHVyeU8+HDlE98Cb+lQrefAlwZm" crossorigin="anonymous"></script>
+    <script>
+     Sentry.replayIntegration({
+         maskAllText: false,
+         blockAllMedia: false,
+     });
+    </script>
     <? if (!isset($_GET['track'])): ?>
     <script defer src="https://umami.jcubic.pl/script.js"
             data-website-id="bb1c5851-93fe-4fce-8209-944c25b8f7be"></script>
