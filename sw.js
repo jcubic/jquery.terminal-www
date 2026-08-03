c01ea1f5f93d9516ce55c8dfea5d7129f3eaaf15
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-08-03 18:43:10 +0200

    use wayne.force()

--- sw.js
+++ sw.js
@@ -15,10 +15,4 @@ app.get('https://browser.sentry-cdn.com/*', async (req, res) => {
     }
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
-});
-
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
-});
+wayne.force();
