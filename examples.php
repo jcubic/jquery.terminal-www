798acd8171eb95c3d7150eb29a340fabe80fd3bb
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-09-15 14:04:11 +0200

    add link to Hacking Cafe github

--- examples.php
+++ examples.php
@@ -1534,6 +1534,7 @@ history.pushState(save_state.length-1, null, '&lt;NEW URL&gt;');</pre>
           <li><a href="https://codepen.io/jcubic/pen/BwBYOZ">Vintage Fake Terminal</a>.</li>
           <li><a href="https://codepen.io/jcubic/pen/WZvYGj">OSX, Ubuntu and Windows 10 terminals</a>.</li>
           <li><a href="https://github.com/jcubic/fake-linux-terminal">Fake Linux Terminal</a> &mdash; Demo of SVG laptop with terminal inside. The Terminal have fake Linux with fake in browser filesystem, and common commands.</li>
+          <li><a href="https://github.com/jcubic/hacking.cafe">Hacking Cafe</a> &mdash; a successor to Fake Linux Terminal.</li>
           <li><a href="https://codepen.io/jcubic/pen/jOaoGKJ">Cool multi line prompt</a> - inspired by questionn on Reddit.</li>
           <li><a href="https://codepen.io/jcubic/pen/qPMPOR">Linux like NCurses dialog with cmd plugin and bit of css</a>.</li>
           <li><a href="https://codepen.io/jcubic/pen/dyMVWNX">Terminal with Night Vision Effect</a>.</li>
